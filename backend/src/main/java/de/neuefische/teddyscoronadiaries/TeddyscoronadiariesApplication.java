b8cf2d550311131d81fbb3861d9d5c129318d6d8
Lisa Mittmann <li.mittmann@gmail.com>
2021-03-14 21:16:42 +0100

    Init backend

---
+++ backend/src/main/java/de/neuefische/teddyscoronadiaries/TeddyscoronadiariesApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.teddyscoronadiaries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeddyscoronadiariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeddyscoronadiariesApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/neuefische/teddyscoronadiaries/TeddyscoronadiariesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.teddyscoronadiaries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeddyscoronadiariesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
