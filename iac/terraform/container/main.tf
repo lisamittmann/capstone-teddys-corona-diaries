5a24d7915fc8948b6de7e5f855716d59bead870c
Lisa Mittmann <li.mittmann@gmail.com>
2021-03-19 17:42:17 +0100

    :construction: Fix jvm flags

--- iac/terraform/container/main.tf
+++ iac/terraform/container/main.tf
@@ -5,18 +5,3 @@ resource "google_artifact_registry_repository" "teddys-corona-diaries" {
   repository_id = "capstone-teddys-corona-diaries"
   format = "DOCKER"
 }
-
-resource "google_cloudbuild_trigger" "build-trigger" {
-  project = "capstone-teddys-corona-diaries"
-  name = "capstone-build-trigger"
-  description = "Trigger used to build image for Teddys Corona Diaries"
-  github {
-    owner = "lisamittmann"
-    name = "capstone-teddys-corona-diaries"
-    push {
-      branch =".*"
-    }
-  }
-
-  filename = "cloudbuild.yaml"
-}
