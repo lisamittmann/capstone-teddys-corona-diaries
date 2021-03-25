46f47f14582d2fc96eb1e0ba544a94c3c0c1ead4
Lisa Mittmann <li.mittmann@gmail.com>
2021-03-25 15:46:26 +0100

    :construction: Current state of covid controller

--- backend/src/main/java/de/neuefische/teddyscoronadiaries/controller/CovidController.java
+++ backend/src/main/java/de/neuefische/teddyscoronadiaries/controller/CovidController.java
@@ -1,27 +1,31 @@
 package de.neuefische.teddyscoronadiaries.controller;
 
 import de.neuefische.teddyscoronadiaries.model.covid.IncidenceDetails;
+import de.neuefische.teddyscoronadiaries.rkiapi.model.RkiAttributes;
 import de.neuefische.teddyscoronadiaries.rkiapi.model.RkiIncidenceValue;
 import de.neuefische.teddyscoronadiaries.rkiapi.model.RkiIncidenceWrapper;
 import de.neuefische.teddyscoronadiaries.rkiapi.service.RkiApiService;
 import de.neuefische.teddyscoronadiaries.service.CovidService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/covid")
 public class CovidController {
 
     private final CovidService covidService;
-    private final RkiApiService rkiApiService;
 
     @Autowired
     public CovidController(CovidService covidService, RkiApiService rkiApiService) {
         this.covidService = covidService;
-        this.rkiApiService = rkiApiService;
     }
 
     @GetMapping("{quarantineDay}")
@@ -29,8 +33,4 @@ public class CovidController {
         return covidService.getSevenDayIncidenceForQuarantineDay(quarantineDay);
     }
 
-    @GetMapping("province/{province}")
-    public RkiIncidenceWrapper getIncidenceValue(@PathVariable String province){
-        return rkiApiService.getIncidenceValueForProvince(province);
-    }
 }
