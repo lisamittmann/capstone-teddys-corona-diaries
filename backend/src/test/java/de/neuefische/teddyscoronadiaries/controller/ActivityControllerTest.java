20f38b25a66739e9affd64681d45b5f35a1271e8
Lisa Mittmann <li.mittmann@gmail.com>
2021-04-10 21:18:27 +0200

    :bug: Fix activity controller tests

--- backend/src/test/java/de/neuefische/teddyscoronadiaries/controller/ActivityControllerTest.java
+++ backend/src/test/java/de/neuefische/teddyscoronadiaries/controller/ActivityControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
 
 import java.util.List;
 
@@ -21,6 +22,7 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"security.jwt.secret=supertestsecret"})
 class ActivityControllerTest {
 
     @LocalServerPort
